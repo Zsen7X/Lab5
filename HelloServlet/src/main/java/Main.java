24bda8827482301d8b48f5aaccc41ff41ffaf02a
Zsen7X <103977981+Zsen7X@users.noreply.github.com>
2024-02-06 11:56:06 +0700

    Add files via upload

---
+++ HelloServlet/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import javax.ws.rs.client.Client;
+
+import org.glassfish.jersey.client.ClientResponse;
+
+public class Main {
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	}
+}
